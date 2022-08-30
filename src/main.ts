7bc6ca50f749e24916cdcfe62375b4bc651a16a2
Juan Cruz Gonzalez <juancruz.gonfer@gmail.com>
2022-08-30 01:30:36 -0300

    create abstract common entity

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from '@rs-target/app.module';
+import validationOptions from '@common/utils/validation-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const reflector = app.get(Reflector);
+
   app.setGlobalPrefix(configService.get<string>('app.apiPrefix'), {
     exclude: ['/'],
   });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
   await app.listen(configService.get<number>('app.port'));
 }
 bootstrap();
