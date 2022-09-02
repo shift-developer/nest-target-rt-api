1303f98264226aa1ded4bc0de06cc69890f98b47
Juan Cruz Gonzalez <juancruz.gonfer@gmail.com>
2022-09-02 12:12:51 -0300

    fix: path alias app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { AppModule } from '@rs-target/app.module';
+import { AppModule } from '@app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
